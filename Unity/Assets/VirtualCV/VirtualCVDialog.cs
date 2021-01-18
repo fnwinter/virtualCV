793a008739d6a86a612a13e8bfcbe813e2faa88d
JungJik Lee <jungjik.lee@samsung.com>
2021-01-19 01:37:53 +0900

    Add SetupDialog
    - add setup Dialog tempo file

---
+++ Unity/Assets/VirtualCV/VirtualCVDialog.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class VirtualCVSetUp : EditorWindow
+{
+    string myString = "Hello World";
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("virtualCV/Setup")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        VirtualCVSetUp window = (VirtualCVSetUp)EditorWindow.GetWindow(typeof(VirtualCVSetUp));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        myString = EditorGUILayout.TextField("Text Field", myString);
+
+        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+        myBool = EditorGUILayout.Toggle("Toggle", myBool);
+        myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+        EditorGUILayout.EndToggleGroup();
+    }
+}
