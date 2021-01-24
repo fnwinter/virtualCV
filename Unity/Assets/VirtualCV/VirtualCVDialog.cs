15d5b3404961a4984f89ce6b708b6376f4a7d743
JungJik Lee <jungjik.lee@samsung.com>
2021-01-25 01:13:06 +0900

    update dialog ui

--- Unity/Assets/VirtualCV/VirtualCVDialog.cs
+++ Unity/Assets/VirtualCV/VirtualCVDialog.cs
@@ -10,10 +10,16 @@ using UnityEditor;
 
 public class VirtualCVSetUp : EditorWindow
 {
-    string myString = "Hello World";
-    bool groupEnabled;
-    bool myBool = true;
-    float myFloat = 1.23f;
+    bool usePhysicalCamera = false;
+    bool useDepthCameara = false;
+    bool useStereoCamera = false;
+
+    int textureWidth = 640;
+    int textureHeight = 480;
+    int fov = 90;
+    int fps = 30;
+    float focal_length = 10.0f;
+    float ipd = 0.1f;
 
     // Add menu named "My Window" to the Window menu
     [MenuItem("virtualCV/Setup")]
@@ -26,12 +32,40 @@ public class VirtualCVSetUp : EditorWindow
 
     void OnGUI()
     {
-        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
-        myString = EditorGUILayout.TextField("Text Field", myString);
+        GUILayout.Label("Camera settings", EditorStyles.boldLabel);
 
-        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
-        myBool = EditorGUILayout.Toggle("Toggle", myBool);
-        myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
-        EditorGUILayout.EndToggleGroup();
+        usePhysicalCamera = EditorGUILayout.Toggle("Use physical camera", usePhysicalCamera);
+        useDepthCameara = EditorGUILayout.Toggle("Use depth camera", useDepthCameara);
+
+        EditorGUILayout.Space();
+
+        textureWidth = EditorGUILayout.IntField("Texture width", textureWidth);
+        textureHeight = EditorGUILayout.IntField("Texture height", textureHeight);
+
+        EditorGUILayout.Space();
+
+        fov = EditorGUILayout.IntField("FOV", fov);
+        fps = EditorGUILayout.IntField("FPS", fps);
+        focal_length = EditorGUILayout.FloatField("Focal length", focal_length);
+
+        EditorGUILayout.Space();
+
+        useStereoCamera = EditorGUILayout.Toggle("Use stereo camera", useStereoCamera);
+        ipd = EditorGUILayout.FloatField("Interpupillary distance", ipd);
+
+        EditorGUILayout.Space();
+
+        GUILayout.Button("Save settings");
+        GUILayout.Button("Apply to camera");
+
+        EditorGUILayout.Space();
+
+        int selected = 0;
+        string[] options = new string[]
+        {
+            "Option1", "Option2", "Option3",
+        };
+        selected = EditorGUILayout.Popup("Python script", selected, options);
+        GUILayout.Button("Launch the script");
     }
 }
